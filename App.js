c6e8071092cb58d050104d6afb8054b2dfdd4e4b
perseusss <ed>
2021-08-30 13:45:15 +0300

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { View, Text, Touchable, TouchableOpacity} from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
+import entities from './entities';
+import Physics from './physics';
+
 
 export default function App() {
+  const [running, setRunning] = useState(false)
+  const [gameEngine, setGameEngine] = useState(null)
+  const [currentPoints, setCurrentPoints] = useState(0)
+  useEffect(() => {
+    setRunning(true)
+  }, [])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex:1 }}>
+      <Text style = {{ textAlign:'center', fontSize:40, color:'black', marginTop:30}}>{currentPoints}</Text>
+      <GameEngine
+        ref={(ref) => { setGameEngine(ref) }}
+        style={{ position:'absolute', top: 0, left: 0, right: 0, bottom: 0}}
+        entities = {entities()}
+        systems ={[Physics]}
+        running = {running}
+        onEvent = {(e) => {
+          switch(e.type){
+            case 'game_over':
+              setRunning(false)
+              gameEngine.stop()
+              break;
+            case 'new_point':
+              setCurrentPoints(currentPoints + 1)
+              break;
+          }
+        }}
+      >
+
+      </GameEngine>
+      {
+        !running ? 
+          <View style = {{flex: 1, justifyContent:'center',alignItems:'center'}}>
+            <TouchableOpacity style={{alignItems:'center', justifyContent:'center', backgroundColor:'green', width:'50%', height:'8%'}}
+                              onPress={()=> {
+                                setCurrentPoints(0)
+                                setRunning(true)
+                                gameEngine.swap(entities())
+                              }}
+            >
+              <Text style={{color:'white',fontWeight:'bold'}}>
+                START GAME
+              </Text>
+            </TouchableOpacity>
+
+          </View> : null
+      }
+      <StatusBar style="auto" hidden={true} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
